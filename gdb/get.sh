3d86f5ee0dd675192e0981dcb935e6e89738a12b
Miles Whittaker <mj@whitta.dev>
2020-09-18 13:51:43 -0400

    Added gdb support for pwndbg

--- gdb/get.sh
+++ gdb/get.sh
@@ -3,4 +3,9 @@
 case "$1" in
     "gef") git clone https://github.com/hugsy/gef.git ;;
     "peda") git clone https://github.com/longld/peda.git ;;
+    "pwndbg")
+        git clone https://github.com/pwndbg/pwndbg.git
+        git -C pwndbg submodule update --init --recursive
+        pip install -r pwndbg/requirements.txt
+        ;;
 esac
